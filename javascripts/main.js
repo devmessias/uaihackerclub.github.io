c682afc27af758640cca87be66932806ae70b132
bruno_messias <messias.physics@gmail.com>
2016-11-26 16:33:48 -0200

    cool things

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+var greeting = "Uai Hacker Club",
+    cursor = "<span>&#9646;</span>",
+    cmd = $('#cmd');
+
+  function type(el,text,pos,no){
+    ctext = text.substring(0,pos)+(pos%2?cursor:cursor);
+    $(el).html(ctext);
+    if(pos==text.length){
+        $(el).html(text+cursor);
+    }else{
+        window.setTimeout('type("'+el+'","'+text+'",'+(pos+1)+','+1+');',70);
+    }
+}
+$(document).ready(function(){
+    cmd.append(cursor);
+    type('#cmd',greeting,0);
+})
